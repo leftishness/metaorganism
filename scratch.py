be8ef7576c9ea587f88a0fd0c7d663a893875510
leftishness <ctyler.clark@gmail.com>
2023-05-13 11:07:29 -0400

    Update scratch.py

--- scratch.py
+++ scratch.py
@@ -1,7 +1,5 @@
 import numpy as np
 
-#First, we code two simple networks that compete to achieve a non-equilibrium steady state
-
 # Define the parameters of the system
 N = 10  # number of nodes in each network
 alpha = 0.1  # strength of interaction between the two networks
@@ -11,7 +9,6 @@ gamma2 = 0.2  # damping factor for network 2
 # Initialize the networks with random values
 net1 = np.random.rand(N)
 net2 = np.random.rand(N)
-
 
 # Define the dynamics of the networks
 def update_networks(net1, net2):
@@ -24,18 +21,29 @@ def update_networks(net1, net2):
     # Return the updated networks
     return net1_new, net2_new
 
+# Create a network object with the same dimensions as the first network
+class Module:
+    def __init__(self, n_inputs, n_outputs):
+        self.weights = np.random.randn(n_inputs, n_outputs)
+        self.biases = np.random.randn(n_outputs)
 
-# Run the dynamics of the networks for a fixed number of steps
-for i in range(100):
-    net1, net2 = update_networks(net1, net2)
+    def forward(self, x):
+        y = np.dot(x, self.weights) + self.biases
+        return y
 
+    def backward(self, dy):
+        dx = np.dot(dy, self.weights.T)
+        dw = np.outer(x, dy)
+        db = dy
+        self.weights -= self.lr * dw
+        self.biases -= self.lr * db
 
 # In this example, we define two networks with N nodes each and initialize them with random values. We also define
 # parameters for the strength of interaction between the networks (alpha) and the damping factors for each network
 # (gamma1 and gamma2). We then define a function update_networks that updates each network according to a set of rules
 # that includes interactions with the other network and random fluctuations. This function returns the updated networks
 # as NumPy arrays. Finally, we run the dynamics of the networks for a fixed number of steps using a loop that calls the
-# update_networks function.
+# update_networks function. After the loop, we print the final values of the two networks.
 
 
 # Next we code a machine learning model that uses these 'dissipative' networks as computational building blocks such
@@ -48,12 +56,13 @@ for i in range(100):
 # distribution, such as by forming and dissolving clusters or by adjusting the strength of connections between modules.
 # This would allow the network to adapt to changes in the environment and maintain its non-equilibrium steady state.
 
-
 class Network:
-    def __init__(self, n_inputs, n_outputs, n_modules):
+    def __init__(self, n_inputs, n_outputs, n_modules, n_net1, n_net2):
         self.n_inputs = n_inputs
         self.n_outputs = n_outputs
         self.n_modules = n_modules
+        self.n_net1 = n_net1
+        self.n_net2 = n_net2
 
         # Initialize modules with random weights
         self.modules = [Module(n_inputs, n_outputs) for _ in range(n_modules)]
@@ -61,9 +70,13 @@ class Network:
         # Initialize connections between modules with random strengths
         self.connections = np.random.randn(n_modules, n_modules)
 
-    def forward(self, x):
+    def forward(self, net1, net2):
+        # Update weights of each module based on net1 and net2
+        for i in range(self.n_modules):
+            self.modules[i].update_weights(net1, net2)
+
         # Compute output of each module
-        module_outputs = [module.forward(x) for module in self.modules]
+        module_outputs = [module.forward(self.n_net1 + self.n_net2) for module in self.modules]
 
         # Compute output of each cluster as weighted sum of module outputs
         cluster_outputs = np.dot(self.connections, module_outputs)
@@ -91,9 +104,9 @@ class Network:
         # Update strengths of connections between modules
         self.connections += lr * np.outer(np.mean(self.modules_outputs, axis=1), np.mean(self.modules_outputs, axis=1))
 
-    def adapt(self, x):
+    def adapt(self, net1, net2):
         # Compute current mean of module outputs
-        self.modules_outputs = np.array([module.forward(x) for module in self.modules])
+        self.modules_outputs = np.array([module.forward(self.n_net1 + self.n_net2) for module in self.modules])
         current_mean = np.mean(self.modules_outputs, axis=1)
 
         # Compute new strengths of connections between modules based on change in mean module outputs
